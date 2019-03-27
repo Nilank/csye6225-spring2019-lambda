d68721b7bb7228ccf56f7f660f78bc0eb8ad92e2
Abhinn Ankit <ankit.a@husky.neu.edu>
2019-03-27 19:50:52 -0400

    Update config

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,5 @@
 exports.handler = function(event, context, callback) {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log("working");
-    return "test"; // Echo back the first key value
-    // throw new Error('Something went wrong');
+    return null; // Echo back the first key value
 };
